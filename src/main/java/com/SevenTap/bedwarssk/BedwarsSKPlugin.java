573a83b7458408c9ccfa3727ee72ab40b3263c90
UniMilk <dujinhan2006@outlook.com>
2026-06-29 23:32:38 +0800

    Feat: 新增 Bedwars1058 商店 栏目 和 本地化 自动注册功能，采用复制配置文件方式实现

--- src/main/java/com/SevenTap/bedwarssk/BedwarsSKPlugin.java
+++ src/main/java/com/SevenTap/bedwarssk/BedwarsSKPlugin.java
@@ -6,6 +6,9 @@ import com.SevenTap.bedwarssk.game.GameManager;
 import com.SevenTap.bedwarssk.item.MagicWandManager;
 import com.SevenTap.bedwarssk.listener.MagicWandListener;
 import com.SevenTap.bedwarssk.message.MessageSender;
+import com.SevenTap.bedwarssk.shop.AutoShopRegister;
+import com.andrei1058.bedwars.BedWars;
+import com.andrei1058.bedwars.api.language.Language;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,11 +18,13 @@ public class BedwarsSKPlugin extends JavaPlugin {
     private MessageSender messageSender;
     private CommandHandler commandHandler;
     private MagicWandManager magicWandManager;
+    private AutoShopRegister shopRegister;
 
     @Override
     public void onEnable() {
         // 检查BedWars1058是否加载
-        if (Bukkit.getPluginManager().getPlugin("BedWars1058") == null) {
+        boolean is1058Loaded = Bukkit.getPluginManager().getPlugin("BedWars1058") != null;
+        if (!is1058Loaded) {
             getLogger().warning("BedWars1058未找到！某些功能可能无法正常工作。");
         }
 
@@ -30,6 +35,12 @@ public class BedwarsSKPlugin extends JavaPlugin {
         messageSender = new MessageSender(this, gameManager);
         magicWandManager = new MagicWandManager(this);
         commandHandler = new CommandHandler(gameManager, messageSender, magicWandManager);
+        shopRegister = new AutoShopRegister(this, BedWars.shop, Language.getDefaultLanguage());
+
+        // 自动配置 1058 商店
+        if (is1058Loaded) {
+            shopRegister.register();
+        }
 
         // 注册事件监听器
         Bukkit.getPluginManager().registerEvents(new GameListener(this), this);
---
+++ src/main/java/com/SevenTap/bedwarssk/shop/AutoShopRegister.java
@@ -0,0 +1,54 @@
+package com.SevenTap.bedwarssk.shop;
+
+import com.SevenTap.bedwarssk.BedwarsSKPlugin;
+import com.andrei1058.bedwars.api.language.Language;
+import com.andrei1058.bedwars.shop.ShopManager;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class AutoShopRegister {
+    private final BedwarsSKPlugin plugin;
+    private final FileConfiguration config;
+    private final ShopManager shopManager;
+    private final Language defaultLocale;
+
+    public AutoShopRegister(BedwarsSKPlugin plugin, ShopManager shopManager, Language defaultLocale) {
+        this.plugin = plugin;
+        this.config = plugin.getConfig();
+        this.shopManager = shopManager;
+        this.defaultLocale = defaultLocale;
+    }
+
+    public void register() {
+        plugin.getLogger().info("尝试在 Bedwars1058 商店中注册魔法道具...");
+        boolean isScRegistered = registerShopCategory();
+        boolean isLocaleRegistered = registerDefaultLocale();
+        if (isScRegistered || isLocaleRegistered) {
+            plugin.getLogger().warning("魔法道具注册成功，重启服务器中...");
+            Bukkit.getServer().reload();
+        }
+    }
+
+    private boolean registerShopCategory() {
+        YamlConfiguration shopConfig = shopManager.getYml();
+        if (shopConfig.contains("magic-category")) {
+            plugin.getLogger().info("魔法道具栏已经在 Bedwars1058 商店中注册，跳过自动注册。");
+            return false;
+        }
+        Object magicCategory = config.get("shop.item.magic-category");
+        shopManager.set("magic-category", magicCategory);
+        return true;
+    }
+
+    private boolean registerDefaultLocale() {
+        YamlConfiguration localeConfig = defaultLocale.getYml();
+        if (localeConfig.contains("shop-items-messages.magic-category")) {
+            plugin.getLogger().info("魔法道具本地化已经在 Bedwars1058 本地化文件中注册，跳过自动注册。");
+            return false;
+        }
+        Object magicLocale = config.get("shop.message.magic-category");
+        defaultLocale.set("shop-items-messages.magic-category", magicLocale);
+        return true;
+    }
+}
