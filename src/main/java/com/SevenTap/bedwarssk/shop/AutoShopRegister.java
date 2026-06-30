2e46f025b44a46cfb1e9a0efce240727bda579c5
UniMilk <dujinhan2006@outlook.com>
2026-06-30 21:29:07 +0800

    Fix: 将自动注册魔法道具后的服务器 reload 改为仅重新加载 shop.yml 和 messages_en.yml 配置文件，修复因服务器整体 reload 带来的 bug

--- src/main/java/com/SevenTap/bedwarssk/shop/AutoShopRegister.java
+++ src/main/java/com/SevenTap/bedwarssk/shop/AutoShopRegister.java
@@ -3,7 +3,6 @@ package com.SevenTap.bedwarssk.shop;
 import com.SevenTap.bedwarssk.BedwarsSKPlugin;
 import com.andrei1058.bedwars.api.language.Language;
 import com.andrei1058.bedwars.shop.ShopManager;
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -22,33 +21,31 @@ public class AutoShopRegister {
 
     public void register() {
         plugin.getLogger().info("尝试在 Bedwars1058 商店中注册魔法道具...");
-        boolean isScRegistered = registerShopCategory();
-        boolean isLocaleRegistered = registerDefaultLocale();
-        if (isScRegistered || isLocaleRegistered) {
-            plugin.getLogger().warning("魔法道具注册成功，等待服务器启动完毕后自动重启...");
-            Bukkit.getScheduler().runTaskLater(plugin, Bukkit.getServer()::reload, 3 * 20L);   // 等待服务器加载完成后，再延迟 3 秒重启
-        }
+        registerShopCategory();
+        registerDefaultLocale();
     }
 
-    private boolean registerShopCategory() {
+    private void registerShopCategory() {
         YamlConfiguration shopConfig = shopManager.getYml();
         if (shopConfig.contains("magic-category")) {
             plugin.getLogger().info("魔法道具栏已经在 Bedwars1058 商店中注册，跳过自动注册。");
-            return false;
+            return;
         }
         Object magicCategory = config.get("shop.item.magic-category");
         shopManager.set("magic-category", magicCategory);
-        return true;
+        shopManager.reload();
+        plugin.getLogger().info("魔法道具栏注册成功。");
     }
 
-    private boolean registerDefaultLocale() {
+    private void registerDefaultLocale() {
         YamlConfiguration localeConfig = defaultLocale.getYml();
         if (localeConfig.contains("shop-items-messages.magic-category")) {
             plugin.getLogger().info("魔法道具本地化已经在 Bedwars1058 本地化文件中注册，跳过自动注册。");
-            return false;
+            return;
         }
         Object magicLocale = config.get("shop.message.magic-category");
         defaultLocale.set("shop-items-messages.magic-category", magicLocale);
-        return true;
+        defaultLocale.reload();
+        plugin.getLogger().info("魔法道具本地化注册成功。");
     }
 }
